32662fefc9154bdf1c7b41c9c040aea0cd68eac2
Aliphon Fen <redshiftkeying@gmail.com>
2024-08-17 17:48:50 +0800

    fix: dependency upgrade

--- option/src/App.tsx
+++ option/src/App.tsx
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import createEngine, {
+  DefaultLinkModel,
+  DefaultNodeModel,
+  DiagramModel
+} from '@projectstorm/react-diagrams';
+import { CanvasWidget } from '@projectstorm/react-canvas-core';
+
 import './App.css';
+
+// create an instance of the engine with all the defaults
+const engine = createEngine();
+// node 1
+const node1 = new DefaultNodeModel({
+  name: 'Node 1',
+  color: 'rgb(0,192,255)',
+});
+node1.setPosition(100, 100);
+let port1 = node1.addOutPort('Out');
+
+// node 2
+const node2 = new DefaultNodeModel({
+  name: 'Node 1',
+  color: 'rgb(0,192,255)',
+});
+node2.setPosition(200, 250);
+let port2 = node2.addOutPort('Out');
+
+// link them and add a label to the link
+const link = port1.link<DefaultLinkModel>(port2);
+link.addLabel('Hello World!');
+
+const model = new DiagramModel();
+model.addAll(node1, node2, link);
+engine.setModel(model);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CanvasWidget className='App' engine={engine} />
   );
 }
 
